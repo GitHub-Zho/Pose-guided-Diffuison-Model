4bd52414419e0ef1301d30cae5344026313f6857
zhou <1415512577@qq.com>
2024-04-28 21:33:48 +0100

    update

--- myScripts/v1.5.py
+++ myScripts/v1.5.py
@@ -6,17 +6,20 @@ from diffusers.utils import load_image
 from diffusers.pipelines.text_to_video_synthesis.pipeline_text_to_video_zero import CrossFrameAttnProcessor
 from datetime import datetime
 
-
+# load the base model
 print(os.getcwd())
 model_path = "../checkpoint/"+"majicmixRealistic_v7.safetensors"
 # model_path = "../checkpoint/"+"stable-diffusion-xl-base-1.0"
 
+# load the control net model
 controlnet_path="../checkpoint/"+"control_v11p_sd15_openpose"
 # controlnet_path="../checkpoint/"+"controlnet-openpose-sdxl-1.0"
 
+# load the lora model
 lora_name="Zendaya"+".safetensors"
 lora_path="../checkpoint/loras/"
 
+# load the pose images
 def ensure_dir(file_path):
     directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
@@ -33,32 +36,39 @@ for filename in filenames:
     pose_images.append(load_image(img_path))
 frames_num=len(pose_images)
 
+
+# set the parameters 
 lora_scale=0.8
 repeated_times =10
 num_inference_steps=80
 
 
 
-
+# set the prompts and the negative prompts 
 prompts = ["relastic,extremely detailed,woman,sophisticated,8k uhd,short hair,dancing,beach,white jacket,brown eyes,sunset","relastic,woman, dancing, house, black jacket, dress,long hair"]
 
 negative_prompt ="(bad-hands-5), interlocked fingers, poorly drawn fingers, (easynegative), (worst quality, low quality:2), twins, closed eyes, missing teeth, extra arms, blurry face, fat belly, heterochromia, big ears, bald, brunette, dull hair, furry, zombie, granny, gore, (watermark)"
 
+# add the control net model to base model
 controlnet = ControlNetModel.from_pretrained(controlnet_path, torch_dtype=torch.float16)
 
 
+# set the generator to generator the image at the very same situation (will generate the exact same image)
+generator = torch.Generator("cuda").manual_seed(31)
 
-#generator = torch.Generator("cuda").manual_seed(31)
-
+# load the pipeline and initial some setting
 pipeline = AutoPipelineForText2Image.from_pretrained(model_path,controlnet = controlnet,torch_dtype=torch.float16,requires_safety_checker=False,safety_checker=None).to("cuda")
+
+# add the lora weights to the model
 # pipeline.load_lora_weights(lora_path, weight_name=lora_name)
 
-
+# add the CrossFrame processor to the model
 pipeline.unet.set_attn_processor(CrossFrameAttnProcessor(batch_size=2))
 pipeline.controlnet.set_attn_processor(CrossFrameAttnProcessor(batch_size=2))
 
-
+# generate the images 
 for k in range(repeated_times):
+    # extract the latent spaces and add the latent to other iterations
     latents = torch.randn((1, 4, 64, 64), device="cuda", dtype=torch.float16).repeat(frames_num, 1, 1, 1)
     for i in range(len(prompts)):
         image = pipeline(prompt = [prompts[i]]*frames_num,negative_prompt=[negative_prompt]*frames_num,image = pose_images,num_inference_steps = num_inference_steps,latents = latents).images
